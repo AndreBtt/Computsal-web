57735c50d986444b99454079645fe6e02926c84a
AndreBtt <bittencourtandre@hotmail.com>
2019-04-24 11:49:09 -0300

    admin route

---
+++ controllers/admin.js
@@ -0,0 +1,5 @@
+// let API = "https://floating-reef-49922.herokuapp.com"
+let API = "http://localhost:8080"
+
+exports.index = function(req, res) {
+}
--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,10 @@ router.get('/', isLoggedIn, controller.home)
 router.use('/criarTime', isLoggedIn, user.createTeam)
 router.use('/agendarHorario', isLoggedIn, user.schedule)
 
+// adm routes
+router.use('/admin', isLoggedIn, isAdmin, require('./admin'))
+
+// general routes
 router.use('/times', require('./teams'))
 router.use('/jogosPassados', require('./previousMatches'))
 router.use('/proximosJogos', require('./nextMatches'))
@@ -17,13 +21,28 @@ router.use('/artilharia', require('./score'))
 router.use('/grupos', require('./groups'))
 
 function isLoggedIn(req, res, next) {
-    // if (req.isAuthenticated()) {
-    //    req.isLogged = true
-    //    return next();
+    // if (usario nao esta logado) {
+    //     res.render('home/index', {
+    //         logged : false,
+    //         adm : false
+    //     })
+    //     return
     // }
     req.logged = true
-    req.adm = true
     req.email = "email14"
+    req.adm = true
+    return next()
+}
+
+function isAdmin(req, res, next) {
+    if (!req.adm) {
+        // user is not a adm
+        res.render('home/index', {
+            logged : req.logged,
+            adm : req.adm
+        })
+        return
+    }
     return next()
 }
 
