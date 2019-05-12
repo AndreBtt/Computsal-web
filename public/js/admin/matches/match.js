e04a7c95d3b6459317950d8456accea61cd49df9
AndreBtt <bittencourtandre@hotmail.com>
2019-05-12 17:10:36 -0300

    player add to table

--- public/js/admin/matches/match.js
+++ public/js/admin/matches/match.js
@@ -1,3 +1,8 @@
+let playerSelectedID
+let teamSelectedName
+let team1
+let team2
+
 $(document).ready(function() {
 
     // Close the dropdown if the user clicks outside of it
@@ -24,6 +29,8 @@ $(document).ready(function() {
         document.getElementById('addAction').click();
     })
 
+    team1 = $("#team1Selected").text()
+    team2 = $("#team2Selected").text()
 })
 
 function dropDown() {
@@ -52,16 +59,19 @@ function displayModalTeam(id) {
     }
 
     if(id == 1) {
+        teamSelectedName = team1
         $("#team1Selected").css("color", "#43b699")
         $("#team1Players").show()
     } else {
+        teamSelectedName = team2
         $("#team2Selected").css("color", "#43b699")
         $("#team2Players").show()
     }
 }
 
-function updateCurrPlayer(player) {
+function updateCurrPlayer(player, id) {
     $("#currPlayer").text(player)
+    playerSelectedID = id
 }
 
 function updateTable() {
@@ -73,5 +83,82 @@ function updateTable() {
         return
     }
 
-    console.log(player)
+    let goals = $("#inputGoal").val()
+    let yellow = $("#inputYellow").val()
+    let red = $("#inputRed").val()
+
+    if(goals == "" && yellow == "" && red == "") {
+        $("#warningText").text("*Nenhum campo de ação selecionado")
+        $("#warningText").show()
+        return
+    }
+
+    if(document.getElementById(playerSelectedID) == null) {
+        // this player does not exist yet
+
+        let divRanking = document.createElement("div")
+        divRanking.classList.add("ranking")
+        divRanking.id = playerSelectedID
+
+        let divPlayer = document.createElement("div")
+        divPlayer.classList.add("col-lg-4")
+        divPlayer.classList.add("col-md-4")
+        divPlayer.classList.add("col-xs-4")
+        divPlayer.innerText = player
+
+        let divTeam = document.createElement("div")
+        divTeam.classList.add("col-lg-4")
+        divTeam.classList.add("col-md-4")
+        divTeam.classList.add("col-xs-4")
+        divTeam.innerText = teamSelectedName
+
+        let divGoals = document.createElement("div")
+        divGoals.classList.add("col-lg-1")
+        divGoals.classList.add("col-md-1")
+        divGoals.classList.add("col-xs-1")
+        divGoals.innerText = goals == "" ? 0 : goals
+
+        let divYellow = document.createElement("div")
+        divYellow.classList.add("col-lg-1")
+        divYellow.classList.add("col-md-1")
+        divYellow.classList.add("col-xs-1")
+        divYellow.innerText = yellow == "" ? 0 : yellow
+
+        let divRed = document.createElement("div")
+        divRed.classList.add("col-lg-1")
+        divRed.classList.add("col-md-1")
+        divRed.classList.add("col-xs-1")
+        divRed.innerText = red == "" ? 0 : red 
+
+        let divDelete = document.createElement("div")
+        divDelete.classList.add("col-lg-1")
+        divDelete.classList.add("col-md-1")
+        divDelete.classList.add("col-xs-1")
+        divDelete.setAttribute("style", "margin-bottom: 10px;")
+
+        let img = document.createElement("img")
+        img.classList.add("icon")
+        img.classList.add("deleteButton")
+        img.setAttribute("src", "/img/icons/delete.png")
+        img.setAttribute('onclick','deletePlayer(' + playerSelectedID + ');')
+        divDelete.appendChild(img)
+
+        divRanking.appendChild(divPlayer)
+        divRanking.appendChild(divTeam)
+        divRanking.appendChild(divGoals)
+        divRanking.appendChild(divYellow)
+        divRanking.appendChild(divRed)
+        divRanking.appendChild(divDelete)
+
+        document.getElementById("playersTable").appendChild(divRanking)
+
+    } else {
+        // this player already exists
+        
+
+    }
+}
+
+function deletePlayer(id) {
+    $('#' + id).hide('slow', function(){ $('#' + id).remove(); });
 }
