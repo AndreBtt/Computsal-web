63a9b3e78a5cc952a8d5c1a7130a030d3e8a7b8e
AndreBtt <bittencourtandre@hotmail.com>
2019-06-07 11:26:58 -0300

    change to port 80

--- app.js
+++ app.js
@@ -19,4 +19,4 @@ app.use(bodyParser.json({ type: 'application/json' }))
 app.use(express.static(__dirname + '/public'));
 app.use(require('./routes'))
 
-app.listen(8080, () => console.log('App listening on port 8080!'));
+app.listen(80, () => console.log('App listening on port 8080!'));
